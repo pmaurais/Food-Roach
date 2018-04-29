18a8fed13d219b0862fa41d7c22bfc774aec1f1c
Paul Maurais <pmaurais@gmail.com>
2018-04-29 15:31:35 -0400

    Build Fix

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,55 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-datepicker.DatePicker",
+    "file": "plugins/cordova-plugin-datepicker/www/ios/DatePicker.js",
+    "pluginId": "cordova-plugin-datepicker",
+    "clobbers": [
+      "datePicker"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-keyboard.keyboard",
+    "file": "plugins/cordova-plugin-ionic-keyboard/www/ios/keyboard.js",
+    "pluginId": "cordova-plugin-ionic-keyboard",
+    "clobbers": [
+      "window.Keyboard"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "cordova.exec"
+    ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-datepicker": "0.9.3",
+  "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-ionic-keyboard": "2.0.5",
+  "cordova-plugin-ionic-webview": "1.2.0",
+  "cordova-plugin-splashscreen": "5.0.2",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
--- src/app/EventData.ts
+++ src/app/EventData.ts
@@ -8,7 +8,7 @@ import {Directive, Injectable} from '@angular/core';
 export class EventData {
 
   public EventList=[{image:"http://www.thechinesequest.com/wp-content/uploads/2014/08/Chinese-food-facts.jpg",name:"China Night",date:"Apr 6",type:"Formal",food:"Asian",distance:"600ft"},{image:"https://butterwithasideofbread.com/wp-content/uploads/2013/07/Greek-Chicken-Souvlaki-with-Lemon-Rice.BSB_.IMG_0509.jpg",name:"American Night",date:"Apr 8",type:"Informal",food:"American",distance:"750ft"},{image:"https://05714042a2232aa91807ef46-qgjpdebgroop4m.netdna-ssl.com/wp-content/uploads/2017/05/Hahn-Speaking.png",name:"Scott Hahn Meet and Greet",date:"Apr 12",type:"Informal",food:"American",distance:"750ft"}];
-
+  public FilterList=[{image:"http://www.thechinesequest.com/wp-content/uploads/2014/08/Chinese-food-facts.jpg",name:"China Night",date:"Apr 6",type:"Formal",food:"Asian",distance:"600ft"},{image:"https://butterwithasideofbread.com/wp-content/uploads/2013/07/Greek-Chicken-Souvlaki-with-Lemon-Rice.BSB_.IMG_0509.jpg",name:"American Night",date:"Apr 8",type:"Informal",food:"American",distance:"750ft"},{image:"https://05714042a2232aa91807ef46-qgjpdebgroop4m.netdna-ssl.com/wp-content/uploads/2017/05/Hahn-Speaking.png",name:"Scott Hahn Meet and Greet",date:"Apr 12",type:"Informal",food:"American",distance:"750ft"}];
   public addEvent(image,name,date,type,food,distance){
     this.EventList.push({image:image,name:name,date:date,type:type,food:food,distance:distance});
   };
